41f462a420f0381e87c5a64d669f4b889327d5b3
chandanbihagpatel <chandanbihagpatel@gmail.com>
2022-06-22 14:09:07 -0400

    started populating

--- src/main/java/org/wecancoeit/reviews/Populator.java
+++ src/main/java/org/wecancoeit/reviews/Populator.java
@@ -24,12 +24,49 @@ public class Populator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Hashtag newHashTag = new Hashtag("#New");
-        hashtagRepo.save(newHashTag);
-        Category newCategory = new Category("Roller Coaster");
-        categoryRepo.save(newCategory);
-        Destination newDestination = new Destination("Cedar Point", "123","asdf", 4.5,newCategory,newHashTag);
-        destinationRepo.save(newDestination);
+        Hashtag fun = new Hashtag("#fun");
+        hashtagRepo.save(fun);
+        Category coaster = new Category("Roller Coaster");
+        categoryRepo.save(coaster);
+        Destination cedarPoint = new Destination("Cedar Point", "Cedar Point DrSandusky, OH 44870","This place is absolutely wild", 4.5,coaster,fun);
+        destinationRepo.save(cedarPoint);
+
+        Hashtag thriller = new Hashtag("#thriller");
+        hashtagRepo.save(thriller);
+        Destination sixFlags = new Destination("Six Flags", "13710 Central Ave, Bowie, MD 20721","Great roller coasters for kids and adults", 4.5,coaster,thriller);
+        destinationRepo.save(sixFlags);
+
+        Category waterPark = new Category("Water Parks");
+        categoryRepo.save(waterPark);
+        Destination zoombezi = new Destination("Zoombezi Bay", "4850 Powell Rd, Powell, OH 43065","Great place for family and children outing.", 4.5,waterPark,fun);
+        destinationRepo.save(zoombezi);
+        Destination bigSplash = new Destination("Big Splash", "2831 Southwest Blvd, Grove City, 43123","Good place with Decent price.", 4.2,waterPark,thriller);
+        destinationRepo.save(bigSplash);
+
+        Hashtag competitions = new Hashtag("#Competitive");
+        hashtagRepo.save(competitions);
+        Category fairs = new Category("Fairs");
+        categoryRepo.save(fairs);
+        Destination ohioStateFair = new Destination("Ohio State Fair", "717 E 17th Ave, Columbus, OH 43211","It’s no Pawnee Harvest Festival, but it’ll do!", 3.0,fairs,competitions);
+        destinationRepo.save(ohioStateFair);
+
+        Hashtag globalCuisine = new Hashtag("#GlobalCuisine");
+        hashtagRepo.save(globalCuisine);
+        Destination minnisotaStateFair = new Destination("Minnisota State Fair", "1265 Snelling Ave N, St Paul, MN 55108","Only place that’s not Las Vegas where you can get married, ride a roller coaster,  and win a hot dog eating contest on the same day!", 4.0,fairs,globalCuisine);
+        destinationRepo.save(minnisotaStateFair);
+
+        Hashtag entertaining = new Hashtag("#Entertaining");
+        hashtagRepo.save(entertaining);
+        Category circuses = new Category("Circuses");
+        categoryRepo.save(circuses);
+        Destination gardenBrothers = new Destination("Garden Brothers Circus", "Myakka City, FL 34251","I liked the horse and elephant shows the best, but the motorcycle act was incredible, and the acrobats were all great", 1.5,circuses,globalCuisine);
+
+
+
+
+
+
+
 
 
 
