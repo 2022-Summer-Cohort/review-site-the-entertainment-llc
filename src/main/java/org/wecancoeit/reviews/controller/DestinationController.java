444c560dc8f42ff43cc2a0e4c369558910c8b66d
chandanbihagpatel <chandanbihagpatel@gmail.com>
2022-06-28 15:21:07 -0400

    added hashtag button

--- src/main/java/org/wecancoeit/reviews/controller/DestinationController.java
+++ src/main/java/org/wecancoeit/reviews/controller/DestinationController.java
@@ -3,20 +3,28 @@ package org.wecancoeit.reviews.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.wecancoeit.reviews.model.Destination;
 import org.wecancoeit.reviews.model.Hashtag;
 import org.wecancoeit.reviews.repos.CategoryRepository;
 import org.wecancoeit.reviews.repos.DestinationRepository;
+import org.wecancoeit.reviews.repos.HashtagRepository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/destination")
 
 public class DestinationController {
     private DestinationRepository destinationRepo;
+    private HashtagRepository hashtagRepo;
 
-    public DestinationController(DestinationRepository destinationRepo) {
+    public DestinationController(DestinationRepository destinationRepo, HashtagRepository hashtagRepo) {
         this.destinationRepo = destinationRepo;
+        this.hashtagRepo = hashtagRepo;
     }
 
     @RequestMapping("/{id}")
@@ -24,11 +32,21 @@ public class DestinationController {
         model.addAttribute("destination", destinationRepo.findById(id).get());
         return "destination";
     }
-    @RequestMapping("/{id}/addhashtag")
-    public String addHashTag(@PathVariable Long id){
-        Hashtag firstHashTag = new Hashtag("#New");
+    @PostMapping("/{id}/addhashtag")
+    public String addHashTag(@PathVariable Long id, @RequestParam String hashtag){
         Destination destination = destinationRepo.findById(id).get();
-        destination.addHashtag(firstHashTag);
+        Optional<Hashtag> hashtagOptional = hashtagRepo.findByhashtagDescriptionIgnoreCase(hashtag);
+        if(hashtagOptional.isPresent()){
+            if(!destination.getHashtags().contains(hashtagOptional.get())){
+                destination.addHashtag(hashtagOptional.get());
+            }
+        }
+        else{
+            Hashtag hashtag1 = new Hashtag(hashtag);
+            hashtagRepo.save(hashtag1);
+            destination.addHashtag(hashtag1);
+
+        }
         destinationRepo.save(destination);
         return "redirect:/destination/"+id;
 
--- src/main/java/org/wecancoeit/reviews/repos/HashtagRepository.java
+++ src/main/java/org/wecancoeit/reviews/repos/HashtagRepository.java
@@ -3,6 +3,9 @@ package org.wecancoeit.reviews.repos;
 import org.springframework.data.repository.CrudRepository;
 import org.wecancoeit.reviews.model.Hashtag;
 
+import java.util.Optional;
+
 public interface HashtagRepository extends CrudRepository<Hashtag, Long> {
+    Optional<Hashtag> findByhashtagDescriptionIgnoreCase(String hashtagDescription);
 
 }
